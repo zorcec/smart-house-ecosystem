abc1efcad4b281d62cfdfd83396fb661a12c9325
Tomislav <zorcec@gmail.com>
2018-10-13 20:13:05 +0200

    Snips is showing signs of life

---
+++ main/snips-master/files/start.sh
@@ -0,0 +1,6 @@
+./usr/bin/snips-dialogue
+./usr/bin/snips-audio-server
+./usr/bin/snips-nlu
+./usr/bin/snips-asr
+./usr/bin/snips-analytics
+./usr/bin/snips-hotword
--- tools/scripts/cleanup-docker.sh
+++ tools/scripts/cleanup-docker.sh
@@ -1,4 +1,4 @@
-# docker stop $(docker ps -a -q)
+docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
-docker rmi $(docker ps -q) 
+docker rmi $(docker images -f "dangling=true" -q)
