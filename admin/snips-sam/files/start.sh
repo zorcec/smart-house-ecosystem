035a8e2774168f29df0b0e1ad1588b60a8ff14bc
Tomislav <zorcec@gmail.com>
2018-10-26 21:34:00 +0200

    Changes to configuration + updated assistant

---
+++ admin/snips-sam/files/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sam login
+sam install assistant -i proj_E6gMV1A3wWo0
--- tools/scripts/cleanup-docker.sh
+++ tools/scripts/cleanup-docker.sh
@@ -1,4 +1,4 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
-docker rmi $(docker images -f "dangling=true" -q)
+#docker rmi $(docker images -f "dangling=true" -q)
