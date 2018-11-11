96df5fb57d31433160ffa381f66c8a78e02c4e4a
Tomislav <zorcec@gmail.com>
2018-11-11 12:14:48 +0100

    picotts fallback for central-tts

--- main/snips-satellite/files/voice.sh
+++ main/snips-satellite/files/voice.sh
@@ -4,8 +4,3 @@ TEXT="$1"
 OUTPUT_FILE="$2"
 
 wget "http://central-tts:8081/transform?text=$TEXT&effects=auto-breaths" -O $OUTPUT_FILE || rm -f $OUTPUT_FILE
-
-if [ ! -f $OUTPUT_FILE ]; then
-    echo "Problem with central-tts; using on device synthesis: $OUTPUT_FILE"
-    pico2wave -w $OUTPUT_FILE $TEXT
-fi
