3cfd2d413588ade3bba0ab7ad087243ef29c33cc
Tomislav Zorčec <zorcec@gmail.com>
2018-12-14 20:10:15 +0100

    Making x64 fully working again

--- satellite/snips-satellite/files/voice.sh
+++ satellite/snips-satellite/files/voice.sh
@@ -3,4 +3,4 @@
 TEXT="$1"
 OUTPUT_FILE="$2"
 
-wget "http://central-tts:8081/transform?text=$TEXT&effects=auto-breaths" -O $OUTPUT_FILE || rm -f $OUTPUT_FILE
+wget "http://central-tts:8081/transform?text=$TEXT&effects=auto-breaths" --quiet -O $OUTPUT_FILE || rm -f $OUTPUT_FILE
